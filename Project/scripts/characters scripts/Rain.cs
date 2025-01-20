c92a5dd2d83cb4d01774b00b9c17da78d60d0989
andr-migue <miguelzamora210405@gmail.com>
2025-01-20 12:39:40 -0500

    some changes

--- Project/scripts/characters scripts/Rain.cs
+++ Project/scripts/characters scripts/Rain.cs
@@ -2,12 +2,24 @@ using Godot;
 public partial class Rain : CharacterBody2D {
     [Export] Movement movement;
     [Export] AnimatedSprite2D animatedSprite;
+    [Export] PointLight2D light;
+    [Export] Label CurrentHability;
+    [Export] Label Cooldown;
     private Vector2 inputVector;
+    private float originalLight;
+    private Timer Timer;
+    private Timer Timer2;
+    private bool IsHability = true;
     public override void _Ready() {
         movement.setup(this);
+        originalLight = light.TextureScale;
+        InitTimer();
+        InitTimer2();
     }
     public override void _Process(double delta) {
         Animation();
+        if (!Timer.IsStopped()) CurrentHability.Text = $"Destello: {Timer.TimeLeft:F1}";
+        if (!Timer2.IsStopped()) Cooldown.Text = $"Cooldown: {Timer2.TimeLeft:F1}";
     }
     public override void _PhysicsProcess(double delta) {
         CheckInputVector();
@@ -34,5 +46,37 @@ public partial class Rain : CharacterBody2D {
         Node parent = GetParent();
         if (parent is Player1 player1) inputVector = player1.InputVector;
         else if (parent is Player2 player2) inputVector = player2.InputVector;
+    }
+    public void Hability() {
+        if (IsHability) {
+            light.TextureScale = light.TextureScale * 2;
+            IsHability = false;
+            Timer.Start();
+            Timer2.Start();
+            CurrentHability.Visible = true;
+            Cooldown.Visible = true;
+        }
+    }
+    private void InitTimer() {
+        Timer = new Timer();
+        Timer.WaitTime = 5.0f;
+        Timer.OneShot = true;
+        Timer.Connect("timeout", new Callable(this, nameof(Timeout)));
+        AddChild(Timer);
+    }
+    private void Timeout() {
+        light.TextureScale = originalLight;
+        CurrentHability.Visible = false;
+    }
+    private void InitTimer2() {
+        Timer2 = new Timer();
+        Timer2.WaitTime = 20.0f;
+        Timer2.OneShot = true;
+        Timer2.Connect("timeout", new Callable(this, nameof(Timeout2)));
+        AddChild(Timer2);
+    }
+    private void Timeout2() {
+        IsHability = true;
+        Cooldown.Visible = false;
     }
 }
